b89656d7a2eb4a6552372c5b29e5e1e19dbddb67
Rafa Cruz <rafa.cruz@goodautomation.com>
2024-06-25 09:57:17 -0600

    Add splash screen with Halliburton within.

---
+++ src/Components/Header/index.tsx
@@ -0,0 +1,12 @@
+// import React from "react"
+// import {View, Text} from "react-native"
+
+// function Header(): React.JSX.Element {
+//   return (
+//     <View style={styles.header}>
+//       <Text style={styles.headerText}>Header</Text>
+//     </View>
+//   )
+// }
+
+// export default Header
